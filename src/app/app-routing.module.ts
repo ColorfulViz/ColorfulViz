7c424ae28d4a02d16f7d573a3d74a78ba78545d8
RoxyKrantz <79843602+RoxyKrantz@users.noreply.github.com>
2021-03-02 00:15:29 +0800

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {IndexComponent} from './view/index/index.component';
+import {TemplateComponent} from './view/template/template.component';
+import {LayoutComponent} from './view/layout/layout.component';
+import {MyChartComponent} from './view/my-chart/my-chart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:IndexComponent
+  },
+  {
+    path:'template',
+    component:TemplateComponent
+  },
+  {
+    path:'layout',
+    component:LayoutComponent
+  },
+  {
+    path:'my-chart',
+    component:MyChartComponent
+  },
+  {
+    path:'**',
+    redirectTo:''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './view/index/index.component';
+import { TemplateComponent } from './view/template/template.component';
+import { LayoutComponent } from './view/layout/layout.component';
+import { MyChartComponent } from './view/my-chart/my-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    TemplateComponent,
+    LayoutComponent,
+    MyChartComponent
   ],
   imports: [
     BrowserModule,
